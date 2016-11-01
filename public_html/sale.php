28b7a63b77b0f32dfbf861713b30a941529c24ae
Kyann Brown <kyann.brown@my.simpson.edu>
2016-11-01 12:27:27 -0500

    Changed all pages' nav bars to navigation.php link

--- public_html/sale.php
+++ public_html/sale.php
@@ -34,38 +34,8 @@
 </head>
 
 <body>
-        <!-- Navigation Bar -->
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-
-            <!-- Three line "hamburger" icon for the collapsed navigation -->
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-
-          <!-- Title on nav bar -->
-          <a class="navbar-left"><img src="img/CraftingCorner.png" alt="Main Graphic" width="50" ></a>
-          <a class="navbar-brand" href="index.html">Suzie's Crafting Corner</a>
-        </div>
-
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="directions.html">Directions</a></li>
-            <li><a href="shop.html">Shop</a></li>
-            <li class="active"><a href="sale.html">Sale</a></li>
-            <li><a href="cart.html">Cart <span class="badge">3</span></a></li>
-            <li><a href="login.html">Login</a></li>
-            <li><a href="sign_up.html">Sign Up</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    
+    <?php include "../resources/navigation.php"; ?>
     
     <div class="container">
         <div class="below_menu_container">
