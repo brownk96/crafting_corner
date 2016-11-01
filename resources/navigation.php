524cc1f430b057446009985f310f057ee2e7fffd
Kyann Brown <kyann.brown@my.simpson.edu>
2016-11-01 12:24:49 -0500

    Change menu links to .php for all pages

--- resources/navigation.php
+++ resources/navigation.php
@@ -13,19 +13,19 @@
 
           <!-- Title on nav bar -->
           <a class="navbar-left"><img src="img/CraftingCorner.png" alt="Main Graphic" width="50"></a>
-          <a class="navbar-brand" href="index.html">Suzie's Crafting Corner</a>
+          <a class="navbar-brand" href="index.php">Suzie's Crafting Corner</a>
         </div>
 
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li><a href="index.html">Home</a></li>
-            <li class="active"><a href="about.html">About</a></li>
-            <li><a href="directions.html">Directions</a></li>
-            <li><a href="shop.html">Shop</a></li>
-            <li><a href="sale.html">Sale</a></li>
-            <li><a href="cart.html">Cart <span class="badge">3</span></a></li>
-            <li><a href="login.html">Login</a></li>
-            <li><a href="sign_up.html">Sign Up</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li class="active"><a href="about.php">About</a></li>
+            <li><a href="directions.php">Directions</a></li>
+            <li><a href="shop.php">Shop</a></li>
+            <li><a href="sale.php">Sale</a></li>
+            <li><a href="cart.php">Cart <span class="badge">3</span></a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="sign_up.php">Sign Up</a></li>
           </ul>
         </div>
       </div>
