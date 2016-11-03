e7ba90706d4e413ed96f5a527f25d1c6889a8a9e
Kyann Brown <kyann.brown@my.simpson.edu>
2016-11-03 11:38:49 -0500

    Change div tag back to what it was in cart.php

--- public_html/cart.php
+++ public_html/cart.php
@@ -87,7 +87,7 @@
     
     <button type="button" class="btn btn-info">Checkout</button>
     
-    <div>
+    <div class="col-xs-12 col-md-12">
     <br>
     <?php include "../resources/footer.php"; ?>
     </div>
